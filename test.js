61e3ef5c3de0526f768a8fbb1a8abb4db5bc95de
Carl Nordenfelt <carl@nordenfelt.com>
2017-03-28 12:59:29 +0200

    New lines

--- test.js
+++ test.js
@@ -64,6 +64,7 @@ describe('aws-signature-v4', function() {
   it('should generate a presigned url', function() {
     assert.equal(presignedURL, 'https://examplebucket.s3.amazonaws.com/test.txt?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIOSFODNN7EXAMPLE%2F20130524%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20130524T000000Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=aeeed9bbccd4d02ee5c0109b86d86835f995330da4c265957d157751f604d404');
   });
+
   it('should generate a presigned url with query params', function() {
     var presignedUrlWithQuery = aws.createPresignedS3URL('test.txt', {
       key: accessKey,
